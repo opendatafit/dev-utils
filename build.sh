3eb2725b794554a73d8d7a9dbfd20dbe55c25ece
echus <varvara@echus.co>
2023-01-27 23:15:56 +1100

    Update to new compose syntax
    /bin/bash -> /bin/sh for compatibility

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 # NOTE: Can't currently use --ssh option with docker-compose
 # Need to build opendatafit-store container separately before executing
 # See issue: https://github.com/docker/compose/issues/7184
 
 (cd $OPENDATAFIT_STORE_SRC && ./build_dockerfile.sh)
-COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose build $@
+COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker compose build $@
