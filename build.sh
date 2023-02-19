5845e1885c8c18ac4bb65529ec32343bb1861c97
echus <varvara@echus.co>
2023-02-19 14:27:55 +1100

    Rename environment variables to match repositories
    Move service-specific development environment variable definitions to
    docker-compose from app configs

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 # Need to build opendatafit-store container separately before executing
 # See issue: https://github.com/docker/compose/issues/7184
 
-(cd $OPENDATAFIT_STORE_SRC && ./build_dockerfile.sh)
+(cd $ODF_STORE_SRC && ./build_dockerfile.sh)
 COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker compose build $@
