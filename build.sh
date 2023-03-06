2b2c9695a6dd4952bc5de5aefda48966c36a62dd
echus <varvara@echus.co>
2023-03-06 13:32:50 +1100

    Add ssh build step for users

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 # NOTE: Can't currently use --ssh option with docker-compose
-# Need to build opendatafit-store container separately before executing
+# Need to build store & users containers separately before executing
+# (they pull in private git repo via ssh in pip requirements)
 # See issue: https://github.com/docker/compose/issues/7184
 
 (cd $ODF_STORE_SRC && ./build_dockerfile.sh)
+(cd $ODF_USERS_SRC && ./build_dockerfile.sh)
 COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker compose build $@
