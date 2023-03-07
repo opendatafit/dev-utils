154421b2cefb256daad040b22f28117a99c87572
echus <varvara@echus.co>
2023-03-07 12:12:15 +1100

    Remove workaround for buildkit ssh option

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
-# NOTE: Can't currently use --ssh option with docker-compose
-# Need to build store & users containers separately before executing
-# (they pull in private git repo via ssh in pip requirements)
-# See issue: https://github.com/docker/compose/issues/7184
 
-(cd $ODF_STORE_SRC && ./build_dockerfile.sh)
-(cd $ODF_USERS_SRC && ./build_dockerfile.sh)
-COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker compose build $@
+if ps -p $SSH_AGENT_PID > /dev/null
+then
+    echo "ssh-agent running."
+else
+    echo "ssh-agent not running. Starting ssh-agent."
+    eval $(ssh-agent)
+fi
+
+echo "Ensure you add your key with ssh-add ~/.ssh/[your key] or you will get ssh errors during build."
+
+COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker compose build --ssh default=$SSH_AUTH_SOCK $@
