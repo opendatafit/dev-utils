06fde7a87da367215a6702dc842a7f337bf89ef1
James Wilmot <jameswilmot2000@gmail.com>
2023-04-04 20:23:49 +1000

    Added frontend flake

---
+++ flakes/frontend/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "opendata.fit development environment for the webapp and frontend packages";
+
+  # Use the unstable nixpkgs to use the latest set of node packages
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = [
+          # Set the major version of Node.js
+          pkgs.nodejs-16_x
+
+          pkgs.yarn
+
+          pkgs.nodePackages.typescript
+          # pkgs.nodePackages.typescript-language-server
+        ];
+      };
+    });
+}
