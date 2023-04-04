16020e241ab20a7ef89bce89a2096b0b69de1997
James Wilmot <jameswilmot2000@gmail.com>
2023-04-04 21:10:05 +1000

    Removed experiment in flake defaultPackage

--- flakes/frontend/flake.nix
+++ flakes/frontend/flake.nix
@@ -13,7 +13,6 @@
     (system: let
       pkgs = import nixpkgs {
         inherit system;
-        defaultPackage.x86_64-linux = self.packages.x86_64-linux.frontend;
       };
     in {
       devShells.default = pkgs.mkShell {
