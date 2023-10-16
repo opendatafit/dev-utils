1341dc55f61b29184c605397bd02b323cb109323
James Wilmot <jameswilmot2000@gmail.com>
2023-10-16 16:01:36 +1100

    Revert "25 remove frontend flake"

---
+++ flakes/frontend-bootstrap/flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "opendata.fit flake for bootstrapping";
+
+  # Use the unstable nixpkgs to use the latest set of node packages
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = [
+          # Set the major version of Node.js
+          pkgs.nodejs-18_x
+          pkgs.nodePackages.ember
+          # pkgs.nodePackages.typescript-language-server
+        ];
+      };
+    });
+}
